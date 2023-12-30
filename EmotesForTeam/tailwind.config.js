3818b7f38a2dacb1d7704a279c9e9f1f39976f17
Charles Poulin <74792298+CharlesPoulin@users.noreply.github.com>
2023-12-30 17:38:19 -0500

    ui changement incoming

--- EmotesForTeam/tailwind.config.js
+++ EmotesForTeam/tailwind.config.js
@@ -9,6 +9,16 @@ module.exports = {
                 'sans': ['Roboto', 'sans-serif'],
             },
             colors: {
+                'sky-950': '#09284C',
+                'slate-600': '#30546D',
+                'cyan-700': '#307089', // and '#307189', you might need to resolve if these are meant to be different
+                'teal-500': '#41A0B6',
+                'zinc-500': '#808080',
+                'indigo-400': '#8992E5', // and '#8A93E5', same note as above
+                'slate-400': '#99B0C0',
+                'violet-200': '#D6DAFF',
+                'red-500': '#E35B3D',
+                'rose-500': '#FF2A77',
                 neutral: {
                     100: '#f5f5f5',
                 },
